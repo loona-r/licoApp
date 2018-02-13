c72709f235c478e9abd0bfb7aecc5a8729c39afe
loona-r <loona-r@users.noreply.github.com>
2018-02-13 10:38:32 +0100

    Correction image retour dans la page settings

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,37 +1,24 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { HomePage } from '../home/home';
+import { Component } from "@angular/core";
+import { NavController, NavParams } from "ionic-angular";
+import { HomePage } from "../home/home";
 
-/**
- * Generated class for the SettingsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
-  selector: 'page-settings',
-  templateUrl: 'settings.html',
+  selector: "page-settings",
+  templateUrl: "settings.html"
 })
 export class SettingsPage {
-
   id;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     //this.id = navParams.data.id;
     this.id = 1;
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SettingsPage');
-  }
-
   backPage() {
-    switch(this.id) {
+    switch (this.id) {
       case 1:
         this.navCtrl.push(HomePage);
-      break;
-      
+        break;
+
       /*
       case 2:
 
@@ -56,8 +43,7 @@ export class SettingsPage {
       */
 
       default:
-      break;
+        break;
     }
   }
-
 }
